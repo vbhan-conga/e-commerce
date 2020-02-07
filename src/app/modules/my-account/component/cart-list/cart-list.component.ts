db533c3792a869ffceb4ea3c66088b7ceaefa5f0
Aaron Mcnulty <amcnulty@apttus.com>
2020-02-07 14:07:07 -0600

    DC-4055
    Fixed an issue where the cart list was not updating when a new cart was created.

--- src/app/modules/my-account/component/cart-list/cart-list.component.ts
+++ src/app/modules/my-account/component/cart-list/cart-list.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, TemplateRef, NgZone, OnDestroy } from '@angular/core';
-import { ACondition, Operator } from '@apttus/core';
-import { TableOptions, FilterOptions, TableAction } from '@apttus/elements';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { AObject } from '@apttus/core';
 import { CartService, Cart, PriceService } from '@apttus/ecommerce';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { Observable, Subscription, of } from 'rxjs';
+import { Observable, of, combineLatest } from 'rxjs';
 import * as _ from 'lodash';
 import { TranslateService } from '@ngx-translate/core';
-import { take, mergeMap, map } from 'rxjs/operators';
+import { map, mergeMap, take } from 'rxjs/operators';
+import { TableOptions, TableAction } from '@apttus/elements';
+import { ClassType } from 'class-transformer/ClassTransformer';
 
 /**
  * Cart list Component loads and shows all the carts for logged in user.
@@ -17,120 +18,93 @@ import { take, mergeMap, map } from 'rxjs/operators';
   templateUrl: './cart-list.component.html',
   styleUrls: ['./cart-list.component.scss']
 })
-export class CartListComponent implements OnInit, OnDestroy {
-
-  /**
-   * All carts for logged in user.
-   */
-  cartList$: Observable<Array<Cart>>;
-  currentCart$: Observable<Cart>;
+export class CartListComponent implements OnInit {
   modalRef: BsModalRef;
   message: string;
   loading: boolean = false;
   cart: Cart;
-  cartAggregate: any;
-  cartAggregate$: Observable<any>;
-  currentCart: Cart;
-  /**
-   * Current page used by the pagination component. Default is 1.
-   */
-  currentPage: number = 1;
-  /**
-   * Number of records per page used by the pagination component. Default is 10.
-   */
-  limit: number = 10;
+  view$: Observable<CartListView>;
   /** @ignore */
   type = Cart;
 
-  tableOptions: TableOptions = {
-    columns: [
-      {
-        prop: 'Name'
-      },
-      {
-        prop: 'CreatedDate'
-      },
-      {
-        prop: 'NumberOfItems'
-      },
-      {
-        prop: 'IsActive',
-        label: 'Is Active',
-        value: (record: Cart) => this.isCartActive(this.currentCart, record) ? of('Yes') : of('No')
-      },
-      {
-        prop: 'TotalAmount',
-        label: 'Total Amount',
-        value: (record: Cart) => this.getCartTotal(record)
-      },
-      {
-        prop: 'Status'
-      }
-    ],
-    actions: [
-      {
-        enabled: true,
-        icon: 'fa-check',
-        massAction: false,
-        label: 'Set Active',
-        theme: 'primary',
-        validate: (record: Cart) => this.canActivate(this.currentCart, record),
-        action: (recordList: Array<Cart>) => this.cartService.setCartActive(_.first(recordList)).pipe(map(cart => null))
-      } as TableAction,
-      {
-        enabled: true,
-        icon: 'fa-trash',
-        massAction: true,
-        label: 'Delete',
-        theme: 'danger',
-        validate: (record: Cart) => this.canDelete(record),
-        action: (recordList: Array<Cart>) => this.cartService.deleteCart(recordList).pipe(map(res => null))
-      } as TableAction
-    ],
-    highlightRow: (record: Cart) => of(this.isCartActive(this.currentCart, record))
-  };
-
-  filterOptions: FilterOptions = {
-    visibleFields: [
-      'CreatedDate'
-    ],
-    visibleOperators: [
-      Operator.EQUAL,
-      Operator.LESS_THAN,
-      Operator.GREATER_THAN,
-      Operator.GREATER_EQUAL,
-      Operator.LESS_EQUAL,
-      Operator.IN
-    ]
-  };
-  private readonly subscriptions: Subscription[] = [];
   /**
    * @ignore
    */
-  constructor(private cartService: CartService, public priceService: PriceService, private modalService: BsModalService, private ngZone: NgZone, private translateService: TranslateService) { }
-
+  constructor(private cartService: CartService, public priceService: PriceService,
+      private modalService: BsModalService, private translateService: TranslateService) { }
   /**
    * @ignore
    */
   ngOnInit() {
-    this.currentCart$ = this.cartService.getMyCart();
-    this.subscriptions.push(this.currentCart$.subscribe((currCart) => this.currentCart = currCart));
-    this.cartAggregate$ = this.cartService.query({ conditions: [new ACondition(Cart, 'Id', 'NotNull', null)], aggregate: true });
+    this.loadView();
   }
-
-  /**
-   * It loads all the cart of logged in user for given page number.
-   * @param page Page number to load cart list.
-   */
-  loadCarts(page) {
-    this.cartList$ = this.cartService.getMyCarts(this.limit, page);
+  /** @ignore */
+  loadView() {
+    this.view$ = this.cartService.getMyCart()
+    .pipe(
+      map((currentCart) => {
+        return {
+          tableOptions: {
+            columns: [
+              {
+                prop: 'Name'
+              },
+              {
+                prop: 'CreatedDate'
+              },
+              {
+                prop: 'NumberOfItems'
+              },
+              {
+                prop: 'IsActive',
+                label: 'Is Active',
+                value:(record: Cart) => this.isCartActive(currentCart, record) ? of('Yes') : of('No')
+              },
+              {
+                prop: 'TotalAmount',
+                label: 'Total Amount',
+                value:(record: Cart) => this.getCartTotal(record)
+              },
+              {
+                prop: 'Status'
+              }
+            ],
+            actions: [
+              {
+                enabled: true,
+                icon: 'fa-check',
+                massAction: false,
+                label: 'Set Active',
+                theme: 'primary',
+                validate:(record: Cart) => this.canActivate(currentCart, record),
+                action:(recordList: Array<Cart>) => this.cartService.setCartActive(_.first(recordList)).pipe(map(cart => null))
+              } as TableAction,
+              {
+                enabled: true,
+                icon: 'fa-trash',
+                massAction: true,
+                label: 'Delete',
+                theme: 'danger',
+                validate:(record: Cart) => this.canDelete(record),
+                action:(recordList: Array<Cart>) => {
+                  this.loadView();
+                  return this.cartService.deleteCart(recordList).pipe(map(res => null));
+                }
+              } as TableAction
+            ],
+            highlightRow:(record: Cart) => of(this.isCartActive(currentCart, record))
+          },
+          type: Cart
+        } as CartListView;
+      })
+    );
   }
 
   /**
    * Creates new cart for logged in user based on input.
    * @param template Modal input for taking user inputs for new cart.
    */
-  newCart(template: TemplateRef<any>){
+  newCart(template: TemplateRef<any>) {
     this.cart = new Cart();
     this.message = null;
     this.modalRef = this.modalService.show(template);
@@ -139,13 +113,13 @@ export class CartListComponent implements OnInit, OnDestroy {
   /**
    * @ignore
    */
-  createCart(){
+  createCart() {
     this.loading = true;
-    this.currentPage = 1;
-    this.cartService.createNewCart(this.cart).subscribe(
+    this.cartService.createNewCart(this.cart).pipe(take(1)).subscribe(
       res => {
         this.loading = false;
         this.modalRef.hide();
+        this.loadView();
       },
       err => {
         this.loading = false;
@@ -175,8 +149,10 @@ export class CartListComponent implements OnInit, OnDestroy {
   isCartActive(currentActiveCart: Cart, rowCart: Cart) {
     return (currentActiveCart.Id === rowCart.Id);
   }
-  /** @ignore */
-  ngOnDestroy() {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
-  }
+
+}
+/** @ignore */
+interface CartListView {
+  tableOptions: TableOptions;
+  type: ClassType<AObject>;
 }
