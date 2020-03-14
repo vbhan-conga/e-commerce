e879c563e1c8de95b6fceb8a5ec745370d44237c
Manohar C M <manoharcm@apttus.com>
2020-03-14 06:13:29 +0000

    environment.ts edited online with Bitbucket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment: Configuration = {
   subqueryLimit: 10,
   disableCache: false,
   encryptResponse: false,
-  cartRetryLimit: 3,
+  cartRetryLimit: 10,
   productIdentifier: 'Id',
   type: 'Salesforce',
   debounceTime: 1000,
@@ -24,8 +24,7 @@ export const environment: Configuration = {
   useIndexedDB: false,
   expandDepth: 7,
   hashRouting: false,
-  packageNamespace: '',
-  // *** TODO: Replace with Salesforce environment variables ***
+    // *** TODO: Replace with Salesforce environment variables ***
   storefront: 'E-Commerce',
   organizationId: '00D3I0000008n7g',
   sentryDsn: 'https://6ad10246235742dc89f89b4c3f53f4aa@sentry.io/1230495',
