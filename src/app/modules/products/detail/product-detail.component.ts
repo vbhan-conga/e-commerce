11330d36dfeb4baa226f06128f539f819c5cbb4c
Vishal Bhan <vbhan@apttus.com>
2020-05-05 15:21:43 +0530

    Reverting the old code changes in product details component.

--- src/app/modules/products/detail/product-detail.component.ts
+++ src/app/modules/products/detail/product-detail.component.ts
@@ -60,10 +60,7 @@ export class ProductDetailComponent implements OnInit {
      */
     onConfigurationChange(result: any) {
         this.cartItemList = _.first(result);
-        if ((_.get(result[1], 'optionChanged') || _.get(result[1], 'attributeChanged')) ||
-          (_.get(this.viewState$, 'isCmsEnabled') && _.get(this.viewState$, 'relatedTo'))) {
-          this.configurationChanged = true;
-        }
+        if (_.get(result[1],'optionChanged') || _.get(result[1],'attributeChanged')) this.configurationChanged = true;
     }
 
     /**
