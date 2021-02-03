70f045b253353a44d464e4868a59d50dfdfaddef
Jay Patel <jpatel@conga.com>
2021-02-03 14:48:50 +0530

    Revert back changes for Product build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -27,6 +27,6 @@ export const environment: Configuration = {
   skipRules: false,
   packageNamespace: 'Apttus_WebStore',
   // Salesforce environment variables
-  storefront: 'ECommerce',
+  storefront: 'D-Commerce',
   endpoint: null
 };
