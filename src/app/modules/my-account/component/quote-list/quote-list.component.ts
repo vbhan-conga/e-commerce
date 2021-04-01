acd89ed4ac8e3050a80ce94757804760f3f267b0
vbhan-conga <vbhan@apttus.com>
2021-04-01 12:58:03 +0530

    DC-6507

--- src/app/modules/my-account/component/quote-list/quote-list.component.ts
+++ src/app/modules/my-account/component/quote-list/quote-list.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { QuoteService, Quote, CartService, LocalCurrencyPipe } from '@apttus/ecommerce';
-import { Observable, combineLatest } from 'rxjs';
-import { QuoteActions } from './quote-actions.component';
-import { Router } from '@angular/router';
-import { ACondition, AFilter } from '@apttus/core';
-import { take, map, flatMap } from 'rxjs/operators';
+
+import { Observable, of } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 import * as moment from 'moment';
-import * as _ from 'lodash';
+import { clone, assign, find, get, isArray, groupBy, sumBy, omit, zipObject, mapKeys, mapValues, map, bind, includes } from 'lodash';
+
+import { AFilter } from '@apttus/core';
+import { QuoteService, Quote, LocalCurrencyPipe } from '@apttus/ecommerce';
 import { TableOptions } from '@apttus/elements';
 
 /** @ignore */
@@ -39,7 +39,7 @@ export class QuoteListComponent implements OnInit {
       {
         prop: 'Grand_Total',
         value: (record) => {
-          return this.currencyPipe.transform(_.get(_.find(_.get(record, 'ProposalSummaryGroups'), {LineType : 'Grand Total'}), 'NetPrice'));
+          return this.currencyPipe.transform(get(find(get(record, 'ProposalSummaryGroups'), {LineType : 'Grand Total'}), 'NetPrice'));
         }
       },
       {
@@ -81,48 +81,61 @@ export class QuoteListComponent implements OnInit {
    * Array of quotes for current page number.
    */
   quote: Quote = new Quote();
-  view$: Observable<QuoteListView>;
   minDaysFromDueDate: number = 7;
   maxDaysFromDueDate: number = 14;
-  actionConfiguration: object;
   colorPalette: Array<String> = ['#D22233', '#F2A515', '#6610f2', '#008000', '#17a2b8', '#0079CC', '#CD853F', '#6f42c1', '#20c997', '#fd7e14'];
   filterList: Array<AFilter> = [];
+  totalAmount$: Observable<number>;
+  amountsByStatus$: Observable<number>;
+  totalRecords$: Observable<number>;
+  quotesByStatus$: Observable<number>;
+  quotesByDueDate$: Observable<number>;
 
   /** @ignore */
-  constructor(private quoteService: QuoteService, private cartService: CartService, private router: Router, private currencyPipe: LocalCurrencyPipe) {
-    this.actionConfiguration = new QuoteActions().actionConfiguration;
-  }
+  constructor(private quoteService: QuoteService, private currencyPipe: LocalCurrencyPipe) { }
 
   /** @ignore */
   ngOnInit() {
     this.loadViewData();
   }
 
-  loadViewData() {
-    this.view$ = combineLatest(this.quoteService.query({
+  private loadViewData() {
+    this.quoteService.query({
       aggregate: true,
       groupBy: ['Approval_Stage', 'RFP_Response_Due_Date'],
       filters: this.filterList
-    }),
-    this.quoteService.getGrandTotalByApprovalStage())
+    })
     .pipe(
-      map(([data, totalByStage]) => {
-        return {
-          tableOptions: _.clone(_.assign(this.tableOptions, {filters: this.filterList})),
-          total: _.get(data, 'total_records', _.sumBy(data, 'total_records')),
-          totalAmount: _.get(totalByStage, 'NetPrice', _.sumBy(totalByStage, 'NetPrice')),
-          amountsByStatus: _.isArray(totalByStage)
-          ? _.omit(_.mapValues(_.groupBy(totalByStage, 'Stage'), s => _.sumBy(s, 'NetPrice')), 'null')
-          : _.zipObject([_.get(totalByStage, 'Stage')], _.map([_.get(totalByStage, 'Stage')], key => _.get(totalByStage, 'NetPrice'))),
-          quotesByStatus: _.isArray(data)
-            ? _.omit(_.mapValues(_.groupBy(data, 'Apttus_Proposal__Approval_Stage__c'), s => _.sumBy(s, 'total_records')), 'null')
-            : _.zipObject([_.get(data, 'Apttus_Proposal__Approval_Stage__c')], _.map([_.get(data, 'Apttus_Proposal__Approval_Stage__c')], key => _.get(data, 'total_records'))),
-          quotesByDueDate: _.isArray(data)
-            ? _.omit(_.mapKeys(_.mapValues(_.groupBy(data, 'Apttus_Proposal__RFP_Response_Due_Date__c'), s => _.sumBy(s, 'total_records')), _.bind(this.generateLabels, this)), 'null')
-            : _.zipObject([_.get(data, 'Apttus_Proposal__RFP_Response_Due_Date__c')], _.map([_.get(data, 'Apttus_Proposal__RFP_Response_Due_Date__c')], key => _.get(data, 'total_records')))
-        } as QuoteListView;
+      take(1),
+      tap(data => {
+        this.tableOptions = clone(assign(this.tableOptions, {filters: this.filterList})),
+        this.totalRecords$ = of(get(data, 'total_records', sumBy(data, 'total_records'))),
+        
+        this.quotesByStatus$ = of(
+          isArray(data)
+          ? omit(mapValues(groupBy(data, 'Apttus_Proposal__Approval_Stage__c'), s => sumBy(s, 'total_records')), 'null')
+          : zipObject([get(data, 'Apttus_Proposal__Approval_Stage__c')], map([get(data, 'Apttus_Proposal__Approval_Stage__c')], key => get(data, 'total_records')))
+        ),
+
+        this.quotesByDueDate$ = of(
+          isArray(data)
+          ? omit(mapKeys(mapValues(groupBy(data, 'Apttus_Proposal__RFP_Response_Due_Date__c'), s => sumBy(s, 'total_records')), bind(this.generateLabels, this)), 'null')
+          : zipObject([get(data, 'Apttus_Proposal__RFP_Response_Due_Date__c')], map([get(data, 'Apttus_Proposal__RFP_Response_Due_Date__c')], key => get(data, 'total_records')))
+        )
       })
-    );
+    ).subscribe();
+
+    this.quoteService.getGrandTotalByApprovalStage().pipe(
+      take(1),
+      tap(totalByStage => {
+        this.totalAmount$ = of(get(totalByStage, 'NetPrice', sumBy(totalByStage, 'NetPrice'))),
+
+        this.amountsByStatus$ = of(isArray(totalByStage)
+          ? omit(mapValues(groupBy(totalByStage, 'Stage'), s => sumBy(s, 'NetPrice')), 'null')
+          : zipObject([get(totalByStage, 'Stage')], map([get(totalByStage, 'Stage')], key => get(totalByStage, 'NetPrice')))
+        )
+      })
+    ).subscribe()
   }
   /** @ignore */
   handleFilterListChange(event: any) {
@@ -132,65 +145,20 @@ export class QuoteListComponent implements OnInit {
 
 /**@ignore
  */
-  generateLabels(date): string {
+  private generateLabels(date): string {
     const today = moment(new Date());
     const dueDate = (date) ? moment(date) : null;
     if (dueDate && dueDate.diff(today, 'days') < this.minDaysFromDueDate) {
-      if (!_.includes(this.colorPalette, 'rgba(208, 2, 27, 1)')) this.colorPalette.push('rgba(208, 2, 27, 1)');
+      if (!includes(this.colorPalette, 'rgba(208, 2, 27, 1)')) this.colorPalette.push('rgba(208, 2, 27, 1)');
       return '< ' + this.minDaysFromDueDate + ' Days';
     }
     else if (dueDate && dueDate.diff(today, 'days') > this.minDaysFromDueDate && dueDate.diff(today, 'days') < this.maxDaysFromDueDate) {
-      if (!_.includes(this.colorPalette, 'rgba(245, 166, 35, 1)')) this.colorPalette.push('rgba(245, 166, 35, 1)');
+      if (!includes(this.colorPalette, 'rgba(245, 166, 35, 1)')) this.colorPalette.push('rgba(245, 166, 35, 1)');
       return '< ' + this.maxDaysFromDueDate + ' Days';
     }
     else {
-      if (!_.includes(this.colorPalette, 'rgba(43, 180, 39, 1)')) this.colorPalette.push('rgba(43, 180, 39, 1)');
+      if (!includes(this.colorPalette, 'rgba(43, 180, 39, 1)')) this.colorPalette.push('rgba(43, 180, 39, 1)');
       return '> ' + this.maxDaysFromDueDate + ' Days';
     }
   }
-  /** @ignore */
-  downloadPdf(){
-
-  }
-
-  /** @ignore */
-  delete(quote: Quote){
-
-  }
-
-  /** @ignore */
-  createOrder(){
-
-  }
-
-  /** @ignore */
-  submitForApproval(){
-
-  }
-
-  /** @ignore */
-  activateCartForQuote(quote: Quote){
-    // this.cartService.where(`Apttus_QPConfig__Proposald__c = {0}`, quote.Id)
-    this.cartService.where([new ACondition(this.cartService.type, 'QuoteId', 'Equal', quote.Id)])
-      .pipe(
-        take(1),
-        map(res => res[0]),
-        flatMap(cart => this.cartService.setCartActive(cart))
-      )
-      .subscribe(() => this.router.navigate(['/checkout']));
-  }
-
-  /** @ignore */
-  send(){
-
-  }
-}
-
-/** @ignore */
-export interface QuoteListView {
-  tableOptions: TableOptions;
-  total: number;
-  totalAmount: number;
-  quotesByStatus: object;
-  quotesByDueDate: object;
 }
