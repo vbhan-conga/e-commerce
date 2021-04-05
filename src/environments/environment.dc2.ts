14a5430999ae2bec7b0f968942d89545905bc253
jpatelapttus <jpatel@apttus.com>
2021-04-05 11:42:23 +0530

    Build Fix

--- src/environments/environment.dc2.ts
+++ src/environments/environment.dc2.ts
@@ -27,8 +27,6 @@ export const environment: Configuration = {
   skipRules: false,
   expandDepth: 8,
   hashRouting: true,
-  skipPricing: true,
-  skipRules: false,
   packageNamespace: 'Apttus_WebStore',
   // *** TODO: Replace with Salesforce environment variables ***
   storefront: 'D-Commerce',
