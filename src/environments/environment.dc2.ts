eb369d0cd67f7d86983e190368c3b3efda305130
Christopher Moyle <chmoyle@conga.com>
2021-04-05 12:54:23 -0400

    Build fix

--- src/environments/environment.dc2.ts
+++ src/environments/environment.dc2.ts
@@ -23,12 +23,10 @@ export const environment: Configuration = {
   debounceTime: 1000,
   proxy: 'https://apttus-proxy.herokuapp.com',
   useIndexedDB: true,
-  skipPricing: true,
+  skipPricing: false,
   skipRules: false,
   expandDepth: 8,
   hashRouting: true,
-  skipPricing: true,
-  skipRules: false,
   packageNamespace: 'Apttus_WebStore',
   // *** TODO: Replace with Salesforce environment variables ***
   storefront: 'D-Commerce',
