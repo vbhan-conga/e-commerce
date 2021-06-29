9e11c2e3e2712c0ba112f06e7972e0404d5b8b4d
cvaghela-apttus <cvaghela@apttus.com>
2021-06-29 17:03:38 +0530

    Breaking lines for better readability

--- src/app/modules/installed-products/layout/installed-products-layout.component.ts
+++ src/app/modules/installed-products/layout/installed-products-layout.component.ts
@@ -196,7 +196,12 @@ export class InstalledProductsLayoutComponent implements OnInit, OnDestroy {
               children: [
                 {
                   field: 'AssetLineItems',
-                  filters: [new AFilter(this.assetService.type, [new ACondition(this.assetService.type, 'LineType', 'NotEqual', 'Option'), new ACondition(this.assetService.type, 'PriceType', 'In', ['Recurring', 'Usage']), new ACondition(this.assetService.type, 'BundleAssetId', 'NotNull', null)])]
+                  filters: [
+                    new AFilter(this.assetService.type, [
+                        new ACondition(this.assetService.type, 'LineType', 'NotEqual', 'Option'),
+                        new ACondition(this.assetService.type, 'PriceType', 'In', ['Recurring', 'Usage']),
+                        new ACondition(this.assetService.type, 'BundleAssetId', 'NotNull', null)])
+                  ]
                 }
               ]
             }
